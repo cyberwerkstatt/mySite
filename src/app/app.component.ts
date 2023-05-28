24eee47798da81883e23a9e491c8ea0a1a9023cf
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-05-28 23:07:52 +0200

    added images and started to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sasha-seslija.at';
+export class AppComponent implements OnInit{
+
+  images = ["picture1.jpg", "picture2.jpg", "picture3.jpg"];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();     
+  }
+
+  updateImage(){
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000)
+  }
+
+
+
+
 }
