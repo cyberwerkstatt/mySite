318737e47f3d1ee0bd1b0854cc5d302605e1b3c6
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-06-03 23:23:56 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent{
 
-  
-
-
-
+  constructor(public router: Router) {}
 
 }
+
+
